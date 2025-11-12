8622928e0c217f27752939f7acab1be3da5175fe
João Oliveira <joaopedrooliveira6704@gmail.com>
2025-11-12 11:59:55 +0000

    feat - add tailwind to project

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
-<script setup></script>
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1 class="text-3xl font-bold underline text-red-400">Tailwind!</h1>
 </template>
 
 <style scoped></style>
