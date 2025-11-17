5595c2199885089be69f18b6222ab78e5bcdbc3c
João Oliveira <joaopedrooliveira6704@gmail.com>
2025-11-17 15:44:20 +0000

    feat-ustore : create userStore and first test

---
+++ src/stores/users.js
@@ -0,0 +1,33 @@
+import { defineStore } from "pinia";
+
+export const useUsersStore = defineStore("users", {
+  state: () => ({
+    users: [
+      {
+        username: "joao",
+        firstName: "João",
+        lastName: "Oliveira",
+        createdAt: "2025-05-24",
+        income: 12000,
+        objectives: [1, 2],
+        expenses: [1, 3, 4, 5],
+        friends: ["miguel"],
+      },
+      {
+        username: "miguel",
+        firstName: "Miguel",
+        lastName: "Neto",
+        createdAt: "2023-04-21",
+        income: 18490,
+        objectives: [10, 22],
+        expenses: [12, 31, 41, 51],
+      },
+    ],
+  }),
+  getters: {
+    getUsers: (state) => state.users,
+    getUser: (state) => (username) =>
+      state.users.find((user) => user.username === username),
+  },
+  actions: {},
+});
--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,9 +1,26 @@
 <script>
-export default {};
+import { useUsersStore } from "@/stores/users";
+export default {
+  data() {
+    return {
+      userStore: useUsersStore(),
+      user: {},
+    };
+  },
+
+  mounted() {
+    this.user = this.userStore.getUser("joao");
+  },
+};
 </script>
 
 <template>
-  <h1 class="text-red-400">Profile Page</h1>
+  <h1 class="text-red-400">User Profile</h1>
+  <p>Username: @{{ user.username }}</p>
+  <p>Nome: {{ user.firstName }} {{ user.lastName }}</p>
+  <p>Entrou em: {{ user.createdAt }}</p>
+  <p>Salário: {{ user.income }}€</p>
+  <p v-if="user.friends">Amigos: {{ user.friends.map((friend) => friend) }}</p>
 </template>
 
 <style lang="scss" scoped></style>
