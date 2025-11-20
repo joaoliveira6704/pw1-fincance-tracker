1164d9e4def89136d27072031121a7c34aa44b0f
João Oliveira <joaopedrooliveira6704@gmail.com>
2025-11-20 14:25:54 +0000

    feat-router: add login and register views with routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,19 @@
 import DashboardView from "@/views/DashboardView.vue";
 import ExpensesView from "@/views/ExpensesView.vue";
 import LandingView from "@/views/LandingView.vue";
+import LoginView from "@/views/LoginView.vue";
 import MainView from "@/views/MainView.vue";
 import ObjectivesView from "@/views/ObjectivesView.vue";
 import ProfileView from "@/views/ProfileView.vue";
+import RegisterView from "@/views/RegisterView.vue";
 import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: "/", component: LandingView },
+    { path: "/login", component: LoginView },
+    { path: "/register", component: RegisterView },
     {
       path: "/main",
       component: MainView,
